ee5c0833204003c96a353f4f38e058554bfec544
骑士 <stevin.john@qq.com>
2018-07-10 09:54:45 +0800

    .

---
+++ src/Https/Controllers/TestController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stevin
+ * Date: 2018/7/10
+ * Time: 上午9:52
+ */
+
+namespace Youzan\SDK\Https\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class TestController extends Controller
+{
+
+}
